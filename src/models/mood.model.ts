3a79a624bf48f8445115b6f68a9f47c9b08a4c2a
GyanaprakasKhandual <gyanaprakashkhnadual@gmail.com>
2026-04-10 15:49:51 +0530

    User modal content set

--- src/models/mood.model.ts
+++ src/models/mood.model.ts
@@ -2,13 +2,16 @@
 1. Message
 2. Emoji
 3. Files
+4. Location
+5. Weather
+6. Date
+7. Time
+8. Whole Day Schedule
+9. 2 Hour around Schedule
+10. Whole Day Spotify play history
+11. 2 Hour around Spotify Play history
+12. Whole Day pending todo
+13. 1 hour around pending todo
+14. User's Bio
 
-
-What is goging to check and sotre
-1. That date and Time
-2. search in the calendar which is integrated and store the scehdulet before 2 hours and after 2 hours
-2. search the spotiy which type of song he is lsiten
-3. what is his location
-4. whta is teh weather of the location
-5. what is his bio
- */
+*/
