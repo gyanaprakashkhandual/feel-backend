291bcfbf98c3c6c0a8ce2cb5d841d827f2945048
GyanaprakasKhandual <gyanaprakashkhnadual@gmail.com>
2026-04-10 13:45:46 +0530

    Mood feature file added

---
+++ src/models/mood.model.ts
@@ -0,0 +1,14 @@
+/*
+1. Message
+2. Emoji
+3. Files
+
+
+What is goging to check and sotre
+1. That date and Time
+2. search in the calendar which is integrated and store the scehdulet before 2 hours and after 2 hours
+2. search the spotiy which type of song he is lsiten
+3. what is his location
+4. whta is teh weather of the location
+5. what is his bio
+ */
